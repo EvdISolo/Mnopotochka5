94969b8e22531b19c6c25645611cb98f51b13ee3
EvdIsolo <Sagalakov1998ss@mail.ru>
2025-10-12 21:41:36 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -2,6 +2,9 @@ package org.example;
 
 
 public class PhoneBook {
+public int add (String name, String number) {
+    return 0;
+}
 
 
     public static void main(String[] args) {
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,14 @@
+import org.example.PhoneBook;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook book=new PhoneBook();
+        int count=book.add("Филипп","123456789");
+        assertEquals(1,count);
+
+    }
 }
